55a123d9a6b76043aff2155cfb7575aba49173ef
Joe Staller <joe.staller@prodigygame.com>
2017-06-05 09:55:04 -0400

    Completion logic added

--- js/models.js
+++ js/models.js
@@ -1,1 +1,9 @@
-App.Models.Todo = Backbone.Model.extend({});
+App.Models.Todo = Backbone.Model.extend({
+    defaults:{
+        'isComplete' : false,
+    },
+
+    toggleComplete: function(){
+        this.set({'isComplete': !this.get('isComplete')});
+    }
+});
--- js/views.js
+++ js/views.js
@@ -1,10 +1,22 @@
 App.Views.Todo = Backbone.View.extend({
     template: _.template(
-      '<div>' +
-      '<input type="checkbox" />' + 
-      '<%= description %>' +
+      '<div class="<%= isComplete ? \'complete\' : \'\' %>">' +
+      '<input type="checkbox" <%= isComplete ? \'checked\' : \'\' %> />' + 
+      '<span><%= description %></span>' +
       '</div>'
     ),
+
+    initialize: function(){
+        this.model.on('change', this.render, this);
+    },
+
+    events: {
+        'change input': 'toggleComplete',
+    },
+
+    toggleComplete: function(){
+        this.model.toggleComplete();
+    },
 
     render: function(){
         this.$el.html(this.template(this.model.attributes));
