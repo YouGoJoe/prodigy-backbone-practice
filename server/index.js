622c36237f48533f50df8746d55f7e55574ef4ee
Joe Staller <joestaller23@gmail.com>
2017-06-07 09:53:58 -0400

    GET functionality for a single record and bulk get

--- server/index.js
+++ server/index.js
@@ -5,8 +5,27 @@ const LocalStorage = require('node-localstorage').LocalStorage;
 const localStorage = new LocalStorage('./scratch');
 
 app
+    .get('/', function (request, response) {
+        let allTodos = [];
+        localStorage._keys.forEach(function (todoKey) {
+            let todo = localStorage.getItem(todoKey);
+            if (todo) {
+                allTodos.push(JSON.parse(todo));
+            }
+        }, this);
+
+        response.send(allTodos);
+    })
     .get('/:id', function (request, response) {
-        
+        let key = 'Todo-' + request.params.id;
+
+        let result = localStorage.getItem(key);
+        if (result) {
+            response.send(JSON.parse(result));
+        }
+        else {
+            response.status(400).send("Todo " + request.params.id + " not found.");
+        }
     })
     .post('/', function (request, response) {
 
@@ -22,4 +41,6 @@ app
 
 app.listen(8000, function () {
     console.log('server up');
+
+    localStorage.setItem('Todo-2', JSON.stringify({ foo2: "bar2" }));
 });
