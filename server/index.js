ff5179034cee1a4dd137341929c18cbfcb51c1e8
Joe Staller <joestaller23@gmail.com>
2017-06-07 13:11:13 -0400

    POST/PUT (saving) for Todos one at a time

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+// CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// Parse body params
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set up LocalStorage
 const LocalStorage = require('node-localstorage').LocalStorage;
 const localStorage = new LocalStorage('./scratch');
 
@@ -22,16 +35,37 @@ app
         let result = localStorage.getItem(key);
         if (result) {
             response.send(JSON.parse(result));
-        }
-        else {
+        } else {
             response.status(400).send("Todo " + request.params.id + " not found.");
         }
     })
     .post('/', function (request, response) {
 
+        // Manually generate unique ID
+        let id = localStorage.length + 1
+        let key = 'Todo-' + id;
+        let result = localStorage.getItem(key);
+        while (result) {
+            id = id + 1;
+            key = 'Todo-' + id;
+            result = localStorage.getItem(key);
+        }
+
+        let todo = Object.assign({}, request.body);
+        todo.id = id;
+
+        localStorage.setItem(key, JSON.stringify(todo));
+        response.send(todo);
     })
     .put('/:id', function (request, response) {
-
+        let key = 'Todo-' + request.params.id;
+        let result = localStorage.getItem(key);
+        if (result) {
+            localStorage.setItem(key, JSON.stringify(request.body));
+            response.send(request.body);
+        } else {
+            response.status(400).send("Todo " + request.params.id + " not found.");
+        }
     })
     .delete('/:id', function (request, response) {
         let key = 'Todo-' + request.params.id;
