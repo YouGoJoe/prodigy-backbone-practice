04a862b8c98afef1254d5989e6b40721dfd76522
Joe Staller <joestaller23@gmail.com>
2017-06-08 09:21:19 -0400

    Test for POST route

--- server/test/test.spec.js
+++ server/test/test.spec.js
@@ -29,7 +29,27 @@ describe('Todos', function () {
                     expect(res.status).to.be(200);
                     expect(res.body).to.be.a('array');
                     expect(res.body.length).to.be(0);
-                })
-        })
-    })
+                });
+        });
+    });
+
+    describe('POST Create a Todo', function () {
+        it('it should create a Todo', function () {
+            let todo = { description: 'Testing todo', isComplete: false };
+
+            chai.request(server)
+                .post('/')
+                .send(todo)
+                .end(function(err, res){
+                    expect(res.status).to.be(200);
+                    expect(res.body).to.be.a('object');
+                    expect(res.body).to.have.property('description');
+                    expect(res.body.description).to.equal('Testing todo');
+                    expect(res.body).to.have.property('isComplete');
+                    expect(res.body.description).to.be.false;
+                    expect(res.body).to.have.property('id');
+                    expect(res.body.description).to.equal(1); // Since we flush the DB prior to the tests we can be sure this will be true
+                });
+        });
+    });
 });
